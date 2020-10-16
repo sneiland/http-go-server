c125dfa99f491570166b9d68e32d12834c288928
Steven Neiland <sneiland@hotmail.com>
2020-10-15 21:34:50 -0400

    Add mux router

--- main.go
+++ main.go
@@ -1,64 +1,71 @@
 package main
- 
+
 import (
-    "encoding/json"
-    "fmt"
-    "log"
+	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
+
 	"github.com/gorilla/mux"
 )
- 
+
 // Article ...
 type Article struct {
-	ID     string `json:"Id"`
-    Title  string `json:"Title"`
-    Author string `json:"author"`
-    Link   string `json:"link"`
+	Id     string `json:"Id"`
+	Title  string `json:"Title"`
+	Author string `json:"author"`
+	Link   string `json:"link"`
 }
- 
+
 // Articles ...
 var Articles []Article
- 
+
 func homePage(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Welcome to the HomePage!")
-    fmt.Println("Endpoint Hit: homePage")
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
 }
- 
+
 func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/", homePage)
 	myRouter.HandleFunc("/articles", returnAllArticles)
-	myRouter.HandleFunc("/article/{id}",returnSingleArticle)
+	myRouter.HandleFunc("/article/{id}", returnSingleArticle)
 	log.Fatal(http.ListenAndServe(":8000", myRouter))
 }
- 
+
 func returnAllArticles(w http.ResponseWriter, r *http.Request) {
-    fmt.Println("Endpoint Hit: returnAllArticles")
-    json.NewEncoder(w).Encode(Articles)
+	fmt.Println("Endpoint Hit: returnAllArticles")
+	json.NewEncoder(w).Encode(Articles)
 }
 
 func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
-    vars := mux.Vars(r)
-    key := vars["id"]
- 
-    for _, article := range Articles {
-        if article.ID == key {
-            json.NewEncoder(w).Encode(article)
-        }
-    }
+	vars := mux.Vars(r)
+	key := vars["id"]
+
+	for _, article := range Articles {
+		if article.Id == key {
+			json.NewEncoder(w).Encode(article)
+		}
+	}
 }
- 
+
 func main() {
-    Articles = []Article{
-        Article{Title: "Python Intermediate and Advanced 101",
-            Author: "Arkaprabha Majumdar",
-            Link:   "https://www.amazon.com/dp/B089KVK23P"},
-        Article{Title: "R programming Advanced",
-            Author: "Arkaprabha Majumdar",
-            Link:   "https://www.amazon.com/dp/B089WH12CR"},
-        Article{Title: "R programming Fundamentals",
-            Author: "Arkaprabha Majumdar",
-            Link:   "https://www.amazon.com/dp/B089S58WWG"},
-    }
-    handleRequests()
+	Articles = []Article{
+		Article{
+			Id:     "1",
+			Title:  "Python Intermediate and Advanced 101",
+			Author: "Arkaprabha Majumdar",
+			Link:   "https://www.amazon.com/dp/B089KVK23P"},
+		Article{
+			Id:     "2",
+			Title:  "R programming Advanced",
+			Author: "Arkaprabha Majumdar",
+			Link:   "https://www.amazon.com/dp/B089WH12CR"},
+		Article{
+			Id:     "3",
+			Title:  "R programming Fundamentals",
+			Author: "Arkaprabha Majumdar",
+			Link:   "https://www.amazon.com/dp/B089S58WWG"},
+	}
+	handleRequests()
 }
