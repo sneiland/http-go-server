109e9f3ec3dc1d1e92074a6295611714c619e3c1
Steven Neiland <sneiland@hotmail.com>
2020-10-16 20:38:37 -0400

    Reading from the database

--- main.go
+++ main.go
@@ -1,18 +1,21 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 )
 
 // Article ...
 type Article struct {
-	Id     string `json:"Id"`
-	Title  string `json:"Title"`
+	Id     int    `json:"id"`
+	Title  string `json:"title"`
 	Author string `json:"author"`
 	Link   string `json:"link"`
 }
@@ -29,7 +32,7 @@ func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/", homePage)
 	myRouter.HandleFunc("/articles", returnAllArticles)
-	myRouter.HandleFunc("/article/{id}", returnSingleArticle)
+	myRouter.HandleFunc("/articles/{id}", returnSingleArticle)
 	log.Fatal(http.ListenAndServe(":8000", myRouter))
 }
 
@@ -40,7 +43,11 @@ func returnAllArticles(w http.ResponseWriter, r *http.Request) {
 
 func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	key := vars["id"]
+
+	fmt.Println(vars["id"])
+
+	key, err := strconv.Atoi(vars["id"])
+	ErrorCheck(err)
 
 	for _, article := range Articles {
 		if article.Id == key {
@@ -49,23 +56,65 @@ func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func main() {
-	Articles = []Article{
-		Article{
-			Id:     "1",
-			Title:  "Python Intermediate and Advanced 101",
-			Author: "Arkaprabha Majumdar",
-			Link:   "https://www.amazon.com/dp/B089KVK23P"},
-		Article{
-			Id:     "2",
-			Title:  "R programming Advanced",
-			Author: "Arkaprabha Majumdar",
-			Link:   "https://www.amazon.com/dp/B089WH12CR"},
-		Article{
-			Id:     "3",
-			Title:  "R programming Fundamentals",
-			Author: "Arkaprabha Majumdar",
-			Link:   "https://www.amazon.com/dp/B089S58WWG"},
+func PingDB(db *sql.DB) {
+	err := db.Ping()
+	ErrorCheck(err)
+}
+
+func ErrorCheck(err error) {
+	if err != nil {
+		panic(err.Error())
 	}
+}
+
+func readDB() {
+	db, e := sql.Open("mysql", "webuser:password@/restapi")
+	ErrorCheck(e)
+
+	// close database after all work is done
+	defer db.Close()
+
+	PingDB(db)
+
+	// query all data
+	rows, e := db.Query("select * from articles")
+	ErrorCheck(e)
+
+	// declare empty variable
+	var article = Article{}
+
+	// iterate over rows
+	for rows.Next() {
+		e = rows.Scan(&article.Id, &article.Title, &article.Author, &article.Link)
+		Articles = append(Articles, article)
+		ErrorCheck(e)
+	}
+}
+
+func main() {
+	readDB()
+
+	/*Articles = []Article{
+		Article{
+			id:     1,
+			title:  "Python Intermediate and Advanced 101",
+			author: "Arkaprabha Majumdar",
+			link:   "https://www.amazon.com/dp/B089KVK23P"},
+		Article{
+			id:     2,
+			title:  "R programming Advanced",
+			author: "Arkaprabha Majumdar",
+			link:   "https://www.amazon.com/dp/B089WH12CR"},
+		Article{
+			id:     3,
+			title:  "R programming Fundamentals",
+			author: "Arkaprabha Majumdar",
+			link:   "https://www.amazon.com/dp/B089S58WWG"},
+	}
+
+	for _, article := range Articles {
+		fmt.Println(article)
+	}*/
+
 	handleRequests()
 }
